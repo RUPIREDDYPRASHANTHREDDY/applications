14d5f4ca19412baba6b541cc0bfbefe2b13fa8fa
Prashanth <rupireddyprashanthreddy291@gmail.com>
2022-08-20 21:45:33 +0530

    fotter

--- learning-git/src/App.js
+++ learning-git/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Testing from './Testing';
 import Header from './Header';
+import Fotter from './Fotter';
 
 function App() {
   return (
     <div>
       <Header/>
       <Content/>
+      <Fotter/>
       <Testing/>
       <h1>hello</h1>
     </div>
---
+++ learning-git/src/Fotter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Fotter() {
+  return (
+    <div>This is fotter page</div>
+  )
+}
+
+export default Fotter
